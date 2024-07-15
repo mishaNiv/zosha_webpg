3ec1824457836b160a378026eb16be214a0ad5fd
mishaNiv <misha.nivota@outlook.com>
2024-07-15 14:49:01 +0530

    Added one more piece of content to home page Added snippet for College List page, showcasing first three items in college list and providing link to College List page with full list

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,8 @@
 import '../App.css';
 import Header from '../Components/Header';
 import { Link } from 'react-router-dom';
+import React, {useContext} from 'react';
+import { CollegeListProvider, CollegeListContext } from '../CollegeListContext';
 
 function Home() {
   return ( 
@@ -12,6 +14,8 @@ function Home() {
 }
 
 function FinderPgSnip() {
+
+  const { collegeList } = useContext(CollegeListContext);
   return (
     <div className='homepagesnippets'>
       <div className='exploresnippet'>
@@ -24,9 +28,20 @@ function FinderPgSnip() {
       <div className='findersnippet'>
         <p className='findersniptext'>Find your perfect college by entering your preferences into an AI-powered search bar!</p>
         <Link to="/finder" activeClassName="active">
-            <button className='findersnipbutton'>Go to college search bar</button>
+          <button className='findersnipbutton'>Go to college search bar</button>
         </Link>
       </div>
+      <div className='collistsnippet'>
+        <p className='collistsniptext'>Your college list</p>
+        <Link to="/col-list" activeClassName="active">
+          <button className='collistsnipbutton'>See full college list</button>
+        </Link>
+      </div>
+      <ul className='collistsnip'>
+        {collegeList.slice(0, 3).map((newCollege, index) => (
+          <li key={index}>{newCollege}</li>
+        ))}
+      </ul>
     </div>
   )
 }
